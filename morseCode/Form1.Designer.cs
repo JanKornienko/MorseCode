c4d8d49afc0464cbbb08f13fe238ec80f6810fe7
Jan Kornienko <59119423+kornienkojan@users.noreply.github.com>
2020-08-23 14:47:40 +0200

    Add files via upload

---
+++ morseCode/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace morseCode
+{
+    partial class Morse
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textFile = new System.Windows.Forms.CheckBox();
+            this.button = new System.Windows.Forms.Button();
+            this.radLight = new System.Windows.Forms.RadioButton();
+            this.radSound = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(460, 100);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(12, 119);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(460, 100);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textFile
+            // 
+            this.textFile.AutoSize = true;
+            this.textFile.Location = new System.Drawing.Point(12, 271);
+            this.textFile.Name = "textFile";
+            this.textFile.Size = new System.Drawing.Size(63, 17);
+            this.textFile.TabIndex = 4;
+            this.textFile.Text = "Text file";
+            this.textFile.UseVisualStyleBackColor = true;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 296);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 5;
+            this.button.Text = "Translate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // radLight
+            // 
+            this.radLight.AutoSize = true;
+            this.radLight.Location = new System.Drawing.Point(12, 225);
+            this.radLight.Name = "radLight";
+            this.radLight.Size = new System.Drawing.Size(48, 17);
+            this.radLight.TabIndex = 6;
+            this.radLight.TabStop = true;
+            this.radLight.Text = "Light";
+            this.radLight.UseVisualStyleBackColor = true;
+            // 
+            // radSound
+            // 
+            this.radSound.AutoSize = true;
+            this.radSound.Location = new System.Drawing.Point(12, 248);
+            this.radSound.Name = "radSound";
+            this.radSound.Size = new System.Drawing.Size(56, 17);
+            this.radSound.TabIndex = 7;
+            this.radSound.TabStop = true;
+            this.radSound.Text = "Sound";
+            this.radSound.UseVisualStyleBackColor = true;
+            // 
+            // Morse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 331);
+            this.Controls.Add(this.radSound);
+            this.Controls.Add(this.radLight);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textFile);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Morse";
+            this.Text = "Morse Code Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox textFile;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.RadioButton radLight;
+        private System.Windows.Forms.RadioButton radSound;
+    }
+}
+
---
+++ morseCode/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace morseCode
+{
+    public partial class Morse : Form
+    {
+        public Morse()
+        {
+            InitializeComponent();
+        }
+
+        public void Characters()
+        {
+            string[] morse = new string[130];
+            morse[33] = "--...-"; //!
+            morse[34] = ".-..-."; //"
+            morse[40] = "--..."; //(
+            morse[41] = "-.--.-"; //)
+            morse[44] = "--..--"; //,
+            morse[45] = "-....-"; //-
+            morse[46] = ".-.-.-"; //.
+            morse[47] = "-..-."; //SLASH
+            morse[48] = "-----"; //0
+            morse[49] = ".----"; //1
+            morse[50] = "..---"; //2
+            morse[51] = "...--"; //3
+            morse[52] = "....-"; //4
+            morse[53] = "....."; //5
+            morse[54] = "-...."; //6
+            morse[55] = "--..."; //7
+            morse[56] = "---.."; //8
+            morse[57] = "----."; //9
+            morse[58] = "---..."; //:
+            morse[59] = "-.-.-."; //;
+            morse[63] = "..--.."; //?
+            morse[64] = ".--.-."; //@
+            morse[95] = "..--.-"; //_
+            morse[97] = ".-"; //A
+            morse[98] = "-..."; //B
+            morse[99] = "-.-."; //C
+            morse[100] = "-.."; //D
+            morse[101] = "."; //E
+            morse[102] = "..-."; //F
+            morse[103] = "--."; //G
+            morse[104] = "...."; //H
+            morse[105] = ".."; //I
+            morse[106] = ".---"; //J
+            morse[107] = "-.-"; //K
+            morse[108] = ".-.."; //L
+            morse[109] = "--"; //M
+            morse[110] = "-."; //N
+            morse[111] = "---"; //O
+            morse[112] = ".--."; //P
+            morse[113] = "--.-"; //Q
+            morse[114] = ".-."; //R
+            morse[115] = "..."; //S
+            morse[116] = "-"; //T
+            morse[117] = "..-"; //U
+            morse[118] = "...-"; //V
+            morse[119] = ".--"; //W
+            morse[120] = "-..-"; //X
+            morse[121] = "-.--"; //Y
+            morse[122] = "--.."; //Z
+
+            string text = textBox1.Text.ToLower(); 
+
+            //Translate to morse code
+            foreach (char a in text)
+            {
+                textBox2.Text += morse[a] + "/";
+            }
+        }
+
+        public async void Light()
+        {
+            //Light show
+            var lightForm = new Form2();
+            lightForm.Show();
+
+            foreach (char one in textBox2.Text)
+            {
+                if (one == 45)
+                {
+                    lightForm.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+                }
+
+                if (one == 46)
+                {
+                    lightForm.BackColor = System.Drawing.SystemColors.ControlLightLight ;
+                }
+
+                if (one == 47)
+                {
+                    lightForm.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+                }
+
+                await Task.Delay(250);
+                lightForm.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+                await Task.Delay(50);
+            }
+
+            lightForm.Close();
+        }
+
+        public async void Sound()
+        {
+            foreach (char two in textBox2.Text)
+            {
+                if (two == 45)
+                {
+                    Console.Beep(500,300);
+                }
+
+                if (two == 46)
+                {
+                    Console.Beep(500,100);
+                }
+
+                if (two == 47)
+                {
+                    await Task.Delay(300);
+                }
+            }
+        }
+
+        public void File()
+        {
+            string time = DateTime.Now.ToString("dd.MM.yyyy-HH.mm.ss") + ".txt"; //Name of file
+
+            FileStream file = new FileStream(time, FileMode.Create);
+            StreamWriter write = new StreamWriter(file);
+
+            write.WriteLine(textBox1.Text);
+            write.WriteLine(textBox2.Text);
+
+            write.Close();
+            file.Close();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            Characters();
+
+            if (radSound.Checked)
+            {
+                Sound();
+            }
+
+            if (textFile.Checked)
+            {
+                File();
+            }
+
+            if (radLight.Checked)
+            {
+                Light();
+            }
+        }
+    }
+}
